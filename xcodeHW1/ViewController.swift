aa9417396a0a87a363dac9f60caff12f5593b945
Saidbeg-I <saido1996@mail.ru>
2022-08-10 20:58:24 +0300

    setup Slider colors

--- xcodeHW1/ViewController.swift
+++ xcodeHW1/ViewController.swift
@@ -8,87 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var gratInclusionButton: UIButton!
+// Mark - Outlets
+    @IBOutlet var colorView: UIView!
     
-    @IBOutlet var greenColorView: UIView!
-    @IBOutlet var yellowColorView: UIView!
-    @IBOutlet var redColorView: UIView!
-    var sumPressedButton = 1
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redColorView.alpha = 0.5
-        yellowColorView.alpha = 0.5
-        greenColorView.alpha = 0.5
+        colorView.layer.cornerRadius = 15
         
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
         
-        redColorView.layer.cornerRadius = 55
-        yellowColorView.layer.cornerRadius = 55
-        greenColorView.layer.cornerRadius = 55
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        redLabel.text = String(format: "%.2f",redSlider.value)
+        greenLabel.text = String(format: "%.2f",greenSlider.value)
+        blueLabel.text = String(format: "%.2f",blueSlider.value)
         
-        gratInclusionButton.layer.cornerRadius = 20
     }
-
-    @IBAction func includesColorPressed() {
-        if sumPressedButton == 1 {
-            gratInclusionButton.setTitle("Next", for: .normal)
-            redColorView.alpha = 1
-            sumPressedButton += 1
-        } else if redColorView.alpha == 1  {
-            redColorView.alpha = 0.5
-            yellowColorView.alpha = 1
-        } else if yellowColorView.alpha == 1 {
-            yellowColorView.alpha = 0.5
-            greenColorView.alpha = 1
-        } else if greenColorView.alpha == 1 {
-            greenColorView.alpha = 0.5
-            redColorView.alpha = 1
-        }
-       
+// Mark: - IBActon
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+            )
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
     }
-        
-       /* switch sumPressedButton {
-        case 9 :
-            gratInclusionButton.setTitle("Next", for: .normal)
-            redColorView.alpha = 1
-            sumPressedButton -= 1
-        case 8 :
-            redColorView.alpha = 0.5
-            yellowColorView.alpha = 1
-            sumPressedButton -= 1
-        case 7:
-            yellowColorView.alpha = 0.5
-            greenColorView.alpha = 1
-            sumPressedButton -= 1
-        case 6:
-            redColorView.alpha = 1
-            greenColorView.alpha = 0.5
-            sumPressedButton -= 1
-        case 5:
-            redColorView.alpha = 0.5
-            yellowColorView.alpha = 1
-            sumPressedButton -= 1
-        case 4:
-            yellowColorView.alpha = 0.5
-            greenColorView.alpha = 1
-            sumPressedButton -= 1
-        case 3:
-            greenColorView.alpha = 0.5
-            redColorView.alpha = 1
-            sumPressedButton -= 1
-        case 2:
-            redColorView.alpha = 0.5
-            yellowColorView.alpha = 1
-            sumPressedButton -= 1
-        default:
-            greenColorView.alpha = 1
-            yellowColorView.alpha = 0.5
-            sumPressedButton -= 1
-        }
-    } Извините , решил оставить результат предыдущщих усилий :)
-        а новая пришла в голову после сдачи ДЗ.но вы же сказали можно доработать),
-        по моему идеально ) не на радуюсь :)
-        знаю ошибок миллион, возможно было б правильно отработать опционалы 
-   */
 }
+// Mark: - Private methods
